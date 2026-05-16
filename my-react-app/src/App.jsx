b8e246d0598c13f3fa9c8ddc3ee76c0f6a4681ae
Kakashi7860 <sohelshakh91@gmail.com>
2026-05-16 09:36:06 +0530

    first commit

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -10,34 +10,68 @@ function App() {
 
   const [search, setSearch] = useState("");
 
-  const students = [
+  const [studentName, setStudentName] = useState("");
+
+  const [students, setStudents] = useState([
     "Sohail",
     "Ayaan",
     "Ali",
     "Fatima",
     "Ahmed",
     "Zara",
-  ];
+  ]);
 
+  // Add Student
+  const addStudent = () => {
+
+    if (studentName.trim() === "") {
+      return;
+    }
+
+    setStudents([...students, studentName]);
+
+    setStudentName("");
+  };
+
+  // Filter Students
   const filteredStudents = useMemo(() => {
 
     return students.filter((student) =>
       student.toLowerCase().includes(search.toLowerCase())
     );
 
-  }, [search]);
+  }, [search, students]);
 
   return (
-    <div
-      className={darkMode ? "dark" : "light"}
-    >
+    <div className={darkMode ? "dark" : "light"}>
 
       <Navbar />
+
+      <br />
+
+      {/* Add Student */}
+
+      <input
+        type="text"
+        placeholder="Enter Student Name"
+        value={studentName}
+        onChange={(e) => setStudentName(e.target.value)}
+      />
+
+      <button onClick={addStudent}>
+        Add Student
+      </button>
+
+      <br /><br />
+
+      {/* Search */}
 
       <SearchBar
         search={search}
         setSearch={setSearch}
       />
+
+      {/* Student List */}
 
       <StudentList students={filteredStudents} />
 
